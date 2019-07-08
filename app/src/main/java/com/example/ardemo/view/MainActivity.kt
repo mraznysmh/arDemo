c1bb289762c2bcfabf13ecf23a7a8a24ea788188
mrazny <m.razny.smh@gmail.com>
2019-07-08 11:13:33 +0200

    W trakcie ogarniania problemu z fragmentem

--- app/src/main/java/com/example/ardemo/view/MainActivity.kt
+++ app/src/main/java/com/example/ardemo/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.ardemo.view
 
+import android.app.Fragment
+import android.app.FragmentManager
+import android.app.FragmentTransaction
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
@@ -7,23 +10,44 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.ardemo.R
 import com.example.ardemo.viewModel.MainViewModel
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
+    val fragment: ArFragment?=null
+    val fm: FragmentManager=getFragmentManager()
+    val transaction:FragmentTransaction=fm.beginTransaction()
+
+
+//    FragmentManager fm = getSupportFragmentManager();
+//    FragmentTransaction transaction = fm.beginTransaction();
+//    transaction.replace(R.id.contentFragment, fragment);
+//    transaction.commit();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val cameraButton: Button = findViewById(R.id.cameraButton)
 
         cameraButton.setOnClickListener {
-            val arView= Intent(this, arActivity::class.java)
-
-            startActivity(arView)
+//            val arView= Intent(this, arActivity::class.java)
+//            startActivity(arView)
         }
+        supportFragmentManager.inTransaction{
+            replace(R.id.fragmentContainer, arActivity())
+        }
+//        transaction.replace(R.id.fragmentContainer,fragment)
+//        transaction.commit()
+
+    }
+
+    private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
     }
 
 }
--- app/src/main/java/com/example/ardemo/view/arActivity.kt
+++ app/src/main/java/com/example/ardemo/view/arActivity.kt
@@ -1,82 +1,146 @@
 package com.example.ardemo.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import com.crashlytics.android.Crashlytics
 import com.example.ardemo.R
-import com.google.ar.core.ArCoreApk
-import com.google.ar.core.HitResult
-import com.google.ar.core.Plane
-import com.google.ar.core.Session
+import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import java.io.IOException
 
-class arActivity : AppCompatActivity() {
-    private var arFragment: ArFragment? = null
-    var printer: TransformableNode? = null
-    private var printerRenderable: ModelRenderable? = null
+class arActivity : ArFragment() {
+    //private var arFragment: ArFragment? = null
+    //var printer: TransformableNode? = null
+    //private var printerRenderable: ModelRenderable? = null
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ar)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view=super.onCreateView(inflater, container,savedInstanceState)
 
-        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+        arSceneView.isLightEstimationEnabled = false
 
-        enableAR()
+        initializeSession()
 
+        return view
     }
 
+    override fun getSessionConfiguration(session: Session): Config {
 
-    //Plaszczyzna
-    fun enableAR() {  //w BigRepie jest ModelEnum jako argument, tutaj moge dodac filmik <?>
-        val check = ArCoreApk.getInstance().checkAvailability(this)
-
-        if (check.isSupported) {
-            //Budowa modelu 3D:
-//            val modelResource = when (model) {
-//                //ModelEnum.PRINTEREXT -> R.raw.bigrep_pro_lp_v09
-//                //ModelEnum.PRINTERINT -> R.raw.bigrep_pro_lp_v06
-//                //ModelEnum.TEST -> R.raw.low_poly
-//                     }
-//            ModelRenderable.builder()
-//                .setSource(this, modelResource)
-//                .build()
-//                .thenAccept{ renderable -> printerRenderable = renderable }
-//                .exceptionally { throwable ->
-//                    Log.e("render error", "Unable to load Renderable.", throwable)
-//                    //Crashlytics.log(Log.ERROR, "Bigrep", throwable.message)
-//                    null
-//                }
-            //Sprawdzanie powierzchnii
-            arFragment?.setOnTapArPlaneListener { hitResult: HitResult, _: Plane, _: MotionEvent ->
-                // Create the Anchor.
-                val anchor = hitResult.createAnchor()
-                val anchorNode = AnchorNode(anchor)
-                anchorNode.setParent(arFragment?.arSceneView?.scene)
-
-                // Create the transformable andy and add it to the anchor.
-                printer = TransformableNode(arFragment?.transformationSystem)
-                printer?.scaleController?.minScale = 0.2f
-                printer?.setParent(anchorNode)
-               // printer?.renderable = printerRenderable //to raczej nie potrzebne, tylko MovieRenderable potrzebne
-                printer?.select()
-                //TODO:tworzenie sceny/wyświetlanie filmu na scenie<?>
-
-                //printer?.
+        fun loadAugmentedImageBitmap(imageName: String): Bitmap =
+            requireContext().assets.open(imageName).use {
+                return BitmapFactory.decodeStream(it)
             }
-        } else { // Unsupported or unknown.
-            //toast("Device is not supported or unknown")
-            Crashlytics.log(Log.ERROR, "ARDemo2", "Enable AR")
+
+
+        fun setupAugmentedImageDatabase(config: Config, session: Session): Boolean {
+            try {
+                config.augmentedImageDatabase = AugmentedImageDatabase(session).also { db ->
+                    db.addImage(VIDEO1, loadAugmentedImageBitmap(IMG1))
+//                db.addImage(TEST_VIDEO_2, loadAugmentedImageBitmap(TEST_IMAGE_2))
+//                db.addImage(TEST_VIDEO_3, loadAugmentedImageBitmap(TEST_IMAGE_3))
+                }
+                return true
+            } catch (e: IllegalArgumentException) {
+                Log.e(TAG, "Could not add bitmap to augmented image database", e)
+            } catch (e: IOException) {
+                Log.e(TAG, "IO exception loading augmented image bitmap.", e)
+            }
+            return false
+        }
+
+        return super.getSessionConfiguration(session).also {
+            it.lightEstimationMode = Config.LightEstimationMode.DISABLED
+            it.focusMode = Config.FocusMode.AUTO
+
+            if (!setupAugmentedImageDatabase(it, session)) {
+                Toast.makeText(requireContext(), "Failed", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    fun movieOnPlaneAR(){
+    override fun onUpdate(frameTime: FrameTime) {
+        val frame = arSceneView.arFrame
+        if (frame == null || frame.camera.trackingState != TrackingState.TRACKING) {
+            return
+        }
 
+        val updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage::class.java)
+        for (augmentedImage in updatedAugmentedImages) {
+            Log.d(TAG, "${augmentedImage.trackingState.name} :: augmentedImage = ${augmentedImage.name}")
+
+            if (augmentedImage.trackingState == TrackingState.TRACKING) {
+                Log.d(TAG, "Tracking augmented image [${augmentedImage.name}]")
+            }
+        }
     }
+
+    //Plaszczyzna
+//    fun enableAR() {  //w BigRepie jest ModelEnum jako argument, tutaj moge dodac filmik <?>
+//        val check = ArCoreApk.getInstance().checkAvailability(this)
+//
+//        if (check.isSupported) {
+//            //Budowa modelu 3D:
+////            val modelResource = when (model) {
+////                //ModelEnum.PRINTEREXT -> R.raw.bigrep_pro_lp_v09
+////                //ModelEnum.PRINTERINT -> R.raw.bigrep_pro_lp_v06
+////                //ModelEnum.TEST -> R.raw.low_poly
+////                     }
+////            ModelRenderable.builder()
+////                .setSource(this, modelResource)
+////                .build()
+////                .thenAccept{ renderable -> printerRenderable = renderable }
+////                .exceptionally { throwable ->
+////                    Log.e("render error", "Unable to load Renderable.", throwable)
+////                    //Crashlytics.log(Log.ERROR, "Bigrep", throwable.message)
+////                    null
+////                }
+//            //Sprawdzanie powierzchnii
+//            arFragment?.setOnTapArPlaneListener { hitResult: HitResult, _: Plane, _: MotionEvent ->
+//                // Create the Anchor.
+//                val anchor = hitResult.createAnchor()
+//                val anchorNode = AnchorNode(anchor)
+//                anchorNode.setParent(arFragment?.arSceneView?.scene)
+//
+//                // Create the transformable andy and add it to the anchor.
+//                printer = TransformableNode(arFragment?.transformationSystem)
+//                printer?.scaleController?.minScale = 0.2f
+//                printer?.setParent(anchorNode)
+//               // printer?.renderable = printerRenderable //to raczej nie potrzebne, tylko MovieRenderable potrzebne
+//                printer?.select()
+//                //TODO:tworzenie sceny/wyświetlanie filmu na scenie<?>
+//
+//                //printer?.
+//            }
+//        } else { // Unsupported or unknown.
+//            //toast("Device is not supported or unknown")
+//            Crashlytics.log(Log.ERROR, "ARDemo2", "Enable AR")
+//        }
+//    }
+//
+//    fun movieOnPlaneAR(){
+//
+//    }
+    companion object{
+        private const val TAG="arActivity"
+
+        private const val IMG1="black-square.jpg" //TODO obrazek jakis
+        private const val VIDEO1="chicken.mp4" //TODO z downloadsow
+    }
+
 }
 
